5e74d0e48e71d0576c8eab7135a1c71fc0125119
Lilia_Tsaplina <foxtrot7777@mail.ru>
2019-03-26 09:14:31 +0300

    added test annotations

--- src/test/java/com/lesaalle/CIApplicationTest.java
+++ src/test/java/com/lesaalle/CIApplicationTest.java
@@ -1,10 +1,19 @@
 package com.lesaalle;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertEquals;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class CIApplicationTest {
+    @Test
+    public void contextLoads() {
+    }
+
     @Test
     void test() {
         assertEquals(2, 2);
