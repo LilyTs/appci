f2160af9f0784ab992f5f71348ae0f5023c4e474
Lilia_Tsaplina <foxtrot7777@mail.ru>
2019-03-26 09:04:28 +0300

    added test

--- src/test/java/com/lesaalle/CIApplicationTest.java
+++ src/test/java/com/lesaalle/CIApplicationTest.java
@@ -1,7 +1,12 @@
 package com.lesaalle;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class CIApplicationTest {
-
+    @Test
+    void test() {
+        assertEquals(2, 2);
+    }
 }
